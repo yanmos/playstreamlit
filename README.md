dd04b19cf1f9e76f1fc96a2256b4a7752775ba80
yamktm <yam.ktm@gmail.com>
2023-05-28 20:58:52 +0900

    add comment

--- README.md
+++ README.md
@@ -5,9 +5,7 @@
 1. Dwonload template .xlsx file.
 2. Register data array into .xlsx file.
 3. Upload .xlsx file.
-4. Push button.
-5. Make Contour figure into .xlsx file.
-6. Download result file.
-
-
-
+4. Check the check boxes of sheets you would like calc.
+5. Push [Draw Contour] button.
+6. Make Contour figure into .xlsx file.
+7. Push [Download Result file] button and download result file.
--- drawcontour.py
+++ drawcontour.py
@@ -11,24 +11,34 @@ import tempfile
 from openpyxl.drawing.image import Image
 
 TEMPFILENAME = "drawcontour_template.xlsx"
-HEADER_POS = 'C5'
-GRAPH_POS = 'M6'
-GRAPH_POS_DEF = 26
-N_POINTS = 2000
-N_LEVEL = 11     # N + 1
-G_INIT = 0
-G_UPLOADED = 1
-G_END = 10
+HEADER_POS = 'C5'       # excel cell position of parameters
+GRAPH_POS = 'M6'        # excel cell position of drawing graph
+GRAPH_POS_DEF = 26      # excel cell raw offset of drawing graph
+N_POINTS = 2000         # contour resolution of x and y
+N_LEVEL = 11            # color level N + 1
+G_INIT = 0              # general initial state number of process
+G_UPLOADED = 1          # general upload state number of process
+G_END = 10              # general end state number of process
 
+""" main process
+    display screen
+    button, download, upload
+    in
+        state: number of the process index
+    out
+        non
+"""
 def main(state):
     st.session_state['state'] = state
     st.header("Draw contour into .xlsx")
+    # display download button for template file
     with open(TEMPFILENAME, "rb") as fdata:
         st.download_button("Download template .xlsx file",
                         data=fdata,
                         mime='xlsx',
                         file_name=TEMPFILENAME)
     st.session_state['state'] = G_INIT
+    # display file uploader
     upfile = st.file_uploader("Upload xlsx file",
                 type="xlsx",
                 accept_multiple_files=False,
@@ -38,22 +48,34 @@ def main(state):
                 args=None,
                 kwargs=None,
                 disabled=False)
-    if upfile:
+    if upfile:  # after uploading
         st.session_state['state'] = G_UPLOADED
+        # make a tempolary file and drawing contours in it
         with tempfile.NamedTemporaryFile() as tmp:
             st.session_state['state'] = draw_contour(upfile, tmp.name)
             fdata = BytesIO(tmp.read())
+        # after drawing contours, display the download button
         if st.session_state['state'] == G_END:
             st.download_button("Download Result file",
                             data=fdata,
                             mime='xlsx',
                             file_name=upfile.name)
 
+""" drawing contours
+    check the worksheets
+    display checkboxes, buttons
+    in
+        upfile: file like object
+        outfilename: tempolary filename for drawing
+    out
+        res: number of process index
+"""
 def draw_contour(upfile, outfilename):
     wb = px.load_workbook(upfile, data_only=True)
-    sheetnames = wb.sheetnames
+    sheetnames = wb.sheetnames      # get the sheetnames of the workbook
     sheetlist = {}
     res = st.session_state['state']
+    # display the checkboxes of the sheetnames
     for sheetname in sheetnames:
         sheetlist[sheetname] = st.checkbox(sheetname,
                                 value=False, 
@@ -63,6 +85,7 @@ def draw_contour(upfile, outfilename):
                                 args=None, 
                                 kwargs=None, 
                                 disabled=False)
+    # display the button and drawing the contours of checked sheets
     if st.button('Draw Contour'):
         st.write('Drawing...')
         for sheetname in sheetnames:
@@ -71,12 +94,38 @@ def draw_contour(upfile, outfilename):
         res = G_END
     return res
 
+""" drawing a contour
+    save the result workbook
+    in
+        fileobj: file like object
+        in_shname: target sheetname
+        outfilename: tempolary filename for drawing
+    out
+        non
+"""
 def contour(fileobj, in_shname, outfilename):
     wb, ws, x_label, y_label, z_label, x, y, z, cmaps, methods = set_parameter(fileobj, in_shname)
     make_contour(wb, ws, x_label, y_label, z_label, x, y, z, cmaps, methods)
     wb.save(outfilename)
 
+""" set the parameters for drawing contour
+    in
+        in_fpath: file like object
+        in_shname: target sheetname
+    out
+        wb: workbook object
+        ws: worksheet object
+        x_label: x axis label string
+        y_label: y axis label string
+        z_label: z axis label string
+        x: the 1D array of x
+        y: the 1D array of y
+        z: the 1D array of z
+        cmaps: list of color map
+        methods: list of interpolating method
+"""
 def set_parameter(in_fpath, in_shname):
+    # open workbook as data, ignore formula for reading parameters
     wb = px.load_workbook(in_fpath, data_only=True)
     ws = wb[in_shname]
     wb.active = ws
@@ -114,11 +163,29 @@ def set_parameter(in_fpath, in_shname):
                     if is_num(data.value):
                         zi = np.r_[zi, float(data.value)]
                 z = np.r_[z, [zi]]
+    # open workbook as not ignore formula
     wb = px.load_workbook(in_fpath, data_only=False)
     ws = wb[in_shname]
     wb.active = ws
     return wb, ws, x_label, y_label, z_label, x, y, z, cmaps, methods
 
+""" drawing contour
+    interpolate grid as high resolution from input parameter
+    draw the contour from interpolated data.
+    in
+        wb: workbook object
+        ws: worksheet object
+        x_label: x axis label string
+        y_label: y axis label string
+        z_label: z axis label string
+        x: the 1D array of x
+        y: the 1D array of y
+        z: the 1D array of z
+        cmaps: list of color map
+        methods: list of interpolating method
+    out
+        non
+"""
 def make_contour(wb, ws, x_label, y_label, z_label, x, y, z, cmaps, methods):
     wGraph = ws[GRAPH_POS]
     pos = wGraph
@@ -170,6 +237,12 @@ def make_contour(wb, ws, x_label, y_label, z_label, x, y, z, cmaps, methods):
         pos = pos.offset(GRAPH_POS_DEF, 0)
         st.write(fig)
 
+""" check the float type
+    in
+        s: string of float
+    out
+        bool: if s is number then True, not number then False
+"""
 def is_num(s):
     try:
         float(s)
@@ -184,5 +257,8 @@ def is_num(s):
     else:
         return True
 
+"""
+    start
+"""
 if __name__ == '__main__':
     main(G_INIT)
